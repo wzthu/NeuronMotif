7749a01aa495093bf703c3517c532ddf4d4de1a8
Zheng Wei <wzweizheng@qq.com>
2021-09-17 10:57:01 +0800

    update

--- nm-2/DeepSEA/DeepSEA/modeldef.py
+++ nm-2/DeepSEA/DeepSEA/modeldef.py
@@ -48,7 +48,7 @@ from keras.optimizers import SGD
 
 from keras.activations import relu
 
-input_bp = 600
+input_bp = 1000
 conv_kernel_size = 8
 pool_kernel_size = 4
 
@@ -59,37 +59,31 @@ def crelu(x, alpha=0.0, max_value=None, threshold=1e-6):
     return relu(x, alpha, max_value, threshold)
 
 def get_whole_model():
-    input_bp = 600
     seqInput = Input(shape=(input_bp, 4), name='seqInput')
-    seq = Conv1D(300, 19)(seqInput)
-    seq = BatchNormalization()(seq)
-    seq = Activation('relu')(seq)
-    seq = MaxPooling1D(pool_size=3)(seq)
-    seq = Conv1D(200, 11)(seq)
-    seq = BatchNormalization()(seq)
-    seq = Activation('relu')(seq)
-    seq = MaxPooling1D(pool_size=4)(seq)
-    seq = Conv1D(200, 7)(seq)
-    seq = BatchNormalization()(seq)
-    seq = Activation('relu')(seq)
-    seq = MaxPooling1D(pool_size=4)(seq)
+    seq = Conv1D(320, conv_kernel_size,kernel_regularizer=l1l2, kernel_constraint=maxnrom)(seqInput)
+    seq = Activation(crelu)(seq)
+    seq = MaxPooling1D(pool_size=pool_kernel_size,strides=pool_kernel_size)(seq)
+    seq = Dropout(0.2)(seq)
+    seq = Conv1D(480, conv_kernel_size,kernel_regularizer=l1l2, kernel_constraint=maxnrom)(seq)
+    seq = Activation(crelu)(seq)
+    seq = MaxPooling1D(pool_size=pool_kernel_size,strides=pool_kernel_size)(seq)
+    seq = Dropout(0.2)(seq)
+    seq = Conv1D(960, conv_kernel_size,kernel_regularizer=l1l2, kernel_constraint=maxnrom)(seq)
+    seq = Activation(crelu)(seq)
+    seq = Dropout(0.5)(seq)
     seq = Flatten()(seq)
-    seq = Dense(1000)(seq)
-    seq = Activation('relu')(seq)
-    seq = Dropout(0.3)(seq)
-    seq = Dense(1000)(seq)
-    seq = Activation('relu')(seq)
-    seq = Dropout(0.3)(seq)
-    seq = Dense(164)(seq)
+    seq = Dense(925,kernel_regularizer=l1l2, kernel_constraint=maxnrom)(seq)
+    seq = Activation(crelu)(seq)
+    seq = Dense(919,kernel_regularizer=l1l2, kernel_constraint=maxnrom, activity_regularizer=l1_l2(l1=1e-8,l2=0))(seq)
     seq = Activation('sigmoid')(seq)
     return Model(inputs = [seqInput], outputs = [seq])
 
 def get_model_list(layer, kernel, weight_file='weight.hdf5'):
     model = get_whole_model()
     model.load_weights(weight_file)
-    kernel_nb = [300, 200, 200]
-    kernel_sz = [19, 11, 7]
-    pool_sz = [3,4,4]
+    kernel_nb = [320, 480,960]
+    kernel_sz = [8, 8, 8]
+    pool_sz = [4,4,4]
     input_bps = [getRF(i,kernel_sz, pool_sz) for i in range(len(kernel_sz))] # [8, 39, 163]
     input_bp = input_bps[layer-1]
     seqInput = Input(shape=(input_bp, 4), name='subseqInput')
@@ -97,44 +91,35 @@ def get_model_list(layer, kernel, weight_file='weight.hdf5'):
     model_list = []
     act_model_list = []
     out_list = []
-    seq = Conv1D(300, 19)(seqInput)
+    seq = Conv1D(320, conv_kernel_size,kernel_regularizer=l1l2, kernel_constraint=maxnrom)(seqInput)
     act_model_list.append(Model(inputs = [seqInput], outputs = [seq]))
-    seq = BatchNormalization()(seq)
     model_list.append(Model(inputs = [seqInput], outputs = [seq]))
     if layer > 1:
-        seq = Activation('relu')(seq)
-        seq = MaxPooling1D(pool_size=3)(seq)
-        seq = Conv1D(200, 11)(seq)
+        seq = Activation(crelu)(seq)
+        seq = MaxPooling1D(pool_size=pool_kernel_size,strides=pool_kernel_size)(seq)
+        seq = Dropout(0.2)(seq)
+        seq = Conv1D(480, conv_kernel_size,kernel_regularizer=l1l2, kernel_constraint=maxnrom)(seq)
         act_model_list.append(Model(inputs = [seqInput], outputs = [seq]))
-        seq = BatchNormalization()(seq)
         model_list.append(Model(inputs = [seqInput], outputs = [seq]))
     if layer > 2:
-        seq = Activation('relu')(seq)
-        seq = MaxPooling1D(pool_size=4)(seq)
-        seq = Conv1D(200, 7)(seq)
+        seq = Activation(crelu)(seq)
+        seq = MaxPooling1D(pool_size=pool_kernel_size,strides=pool_kernel_size)(seq)
+        seq = Dropout(0.2)(seq)
+        seq = Conv1D(960, conv_kernel_size,kernel_regularizer=l1l2, kernel_constraint=maxnrom)(seq)
         act_model_list.append(Model(inputs = [seqInput], outputs = [seq]))
-        seq = BatchNormalization()(seq)
         model_list.append(Model(inputs = [seqInput], outputs = [seq]))
     if layer > 3:
-        seq = Activation('relu')(seq)
-        seq = MaxPooling1D(pool_size=4)(seq)
+        seq = Activation(crelu)(seq)
+        seq = Dropout(0.5)(seq)
         seq = Flatten()(seq)
-        seq = Dense(1000)(seq)
+        seq = Dense(925,kerel_regularizer=l1l2, kernel_constraint=maxnrom)(seq)
         act_model_list.append(Model(inputs = [seqInput], outputs = [seq]))
         model_list.append(Model(inputs = [seqInput], outputs = [seq]))
     if layer > 4:
-        seq = Activation('relu')(seq)
-        seq = Dropout(0.3)(seq)
-        seq = Dense(1000)(seq)
+        seq = Activation(crelu)(seq)
+        seq = Dense(919,kernel_regularizer=l1l2, kernel_constraint=maxnrom)(seq)
         act_model_list.append(Model(inputs = [seqInput], outputs = [seq]))
         model_list.append(Model(inputs = [seqInput], outputs = [seq]))
-    if layer > 5:
-        seq = Activation('relu')(seq)
-        seq = Dropout(0.3)(seq)
-        seq = Dense(164)(seq)
-        act_model_list.append(Model(inputs = [seqInput], outputs = [seq]))
-        model_list.append(Model(inputs = [seqInput], outputs = [seq]))
-        seq = Activation('sigmoid')(seq)
     out = seq
     for submodel in model_list:
         for i in range(len(submodel.layers)):

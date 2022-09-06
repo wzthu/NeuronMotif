32afbd8321849703096e51feace3008a9ee31747
wzthu <wzweizheng@qq.com>
2022-09-06 08:22:53 +0800

    update comments

--- NeuronMotif/script.sh
+++ NeuronMotif/script.sh
@@ -1,7 +1,19 @@
 
 
 
-# bash run.layer.sh <layer> <the number of kernels> <threads> <optional: the number of flexible motifs>
+# bash run.layer.sh <layer> <the number of kernels> <threads> <optional: the number of hard-syntax motif or motif combination>
+
+#<layer> the layer number
+#<the number of kernels> the number of kernels in the layer
+#<threads> the number of neuron can be demixed at the same time
+#<optional: the number of hard-syntax motif or motif combination>
+#If you know the maximun number of hard-syntax motifs recognized by the neuron in the layer, setting this parameter is benificial to improving motif quality. 
+#Examples:
+#1 hard syntax motif CTCF
+#1 hard syntax motif CTCF-[5bp]-CTCF
+#2 hard-syntax motifs  CTCF-[5bp]-CTCF-[3~5bp] 
+#3 hard-syntax motifs  CTCF-[3~5bp]-CTCF-[3~5bp]
+
 
 
 bash run.layer.sh 1 128 20

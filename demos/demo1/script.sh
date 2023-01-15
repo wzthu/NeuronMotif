25fa1a022fe4f8e06ace86f33ecb3038f009761d
Zheng Wei <wzweizheng@qq.com>
2023-01-15 10:14:06 +0800

    to log

--- demos/demo1/script.sh
+++ demos/demo1/script.sh
@@ -1,9 +1,8 @@
 
 
 
-# bash run.layer.sh <layer> <the number of kernels> <threads> <optional: the number of flexible motifs>
+# bash run.layer.sh <layer> <the number of kernels> <threads> <optional: the number of flexible motifs>  &> .layer<layer>.log
 
 
-bash run.layer.sh 1 3 3
-bash run.layer.sh 2 1 1
-
+bash run.layer.sh 1 3 3 &> .layer1.log
+bash run.layer.sh 2 1 1 &> .layer2.log
--- demos/demo2/script.sh
+++ demos/demo2/script.sh
@@ -1,13 +1,12 @@
 
 
 
-# bash run.layer.sh <layer> <the number of kernels> <threads> <optional: the number of flexible motifs>
+# bash run.layer.sh <layer> <the number of kernels> <threads> <optional: the number of flexible motifs> &> .layer<layer>.log
 
 
-bash run.layer.sh 1 5 28
-bash run.layer.sh 2 5 28
-bash run.layer.sh 3 6 28
-bash run.layer.sh 4 6 28
-bash run.layer.sh 5 1 28 2
-
+bash run.layer.sh 1 5 28   &> .layer1.log
+bash run.layer.sh 2 5 28   &> .layer2.log
+bash run.layer.sh 3 6 28   &> .layer3.log
+bash run.layer.sh 4 6 28   &> .layer4.log
+bash run.layer.sh 5 1 28 2 &> .layer5.log
 
